32c47f1da503e82064d808cf31ffc181c2d4115d
dorot1986 <marszalek.d@gmail.com>
2018-03-25 11:08:47 +0200

    Initial commit

---
+++ app/src/main/java/pl/dmarszalek/tellmeastory/Main.java
@@ -0,0 +1,16 @@
+package pl.dmarszalek.tellmeastory;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Dorota Marszałek on 2018-03-25.
+ */
+
+public class Main extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
